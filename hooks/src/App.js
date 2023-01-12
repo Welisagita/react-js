e476ecbc0c303e738593468ca0aab347d733689b
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2023-01-12 21:45:12 +0700

    update

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,19 +1,26 @@
 
 //parent component
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Introduction from './components/introduction';
 import Profile from './components/profile';
 
 function App() {
   const [mobilSaya, setMobilSaya] = useState(1)
-  
+  const [warnaMobil, setWarnaMobil] = useState("")
 
-  if (mobilSaya <= 0){
-    alert('Stop!');
-  }
+  useEffect (() => {
+
+    if(mobilSaya > 1){
+    setWarnaMobil("Merah")
+    } else if (mobilSaya < 1) {
+      setWarnaMobil("Biru")
+    }
+  }, [mobilSaya]);
+  
 
   return (
     <>
+      <h5>Warna Mobil: {warnaMobil} </h5>
       <h1>Saya memiliki: {mobilSaya} mobil</h1>
       <button onClick={() => setMobilSaya((prev) => prev + 1)}>Tambah Mobil</button>
       <button onClick={() => setMobilSaya((prev) => prev - 1)}>Kurangi Mobil</button>
