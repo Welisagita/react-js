b44020a0f1f751ede64e179ad3f52bca9f2a2433
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2023-01-13 20:58:36 +0700

    update

---
+++ movie-web-api/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>WELI MOVIE MANIA</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
