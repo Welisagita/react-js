d78d64fc0a4a533fe3e0f7424ebfdad0aa5a3442
Weli Sagita <91396511+Welisagita@users.noreply.github.com>
2023-01-15 09:49:44 +0700

    update

--- movie-web-api/src/App.js
+++ movie-web-api/src/App.js
@@ -1,11 +1,35 @@
 
 import './App.css';
+import {getMovieList, searchMovie} from "./api";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    getMovieList()
+  }, [])
+
+  const search = (q) => {
+    console.log({q});
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>WELI MOVIE MANIA</h1>
+        <input 
+        placeholder="Cari film kesayangan.." 
+        className="Movie-search"
+        onChange= {({target}) => search(target.value)} 
+        />
+
+        <div className="Movie-container">
+          <div className="Movie-wrapper">
+              <div className="Movie-title">CONTOH PERTAMA</div>
+                <img className="Movie-img" />
+              <div className="Movie-date">11/12/2023</div>
+              <div className="Movie-rate">8</div>
+          </div>
+        </div>
+
       </header>
     </div>
   );
